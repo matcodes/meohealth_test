11f74668b57c09461c4d6520624720181b5a87d0
matcodes <codemathews@gmail.com>
2025-02-06 23:42:37 +0530

    feat: validation enhanced

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -6,18 +6,36 @@ export interface PrimeRequest extends Request {
   };
 }
 
+class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "INVALID_INPUT";
+  }
+}
+
 export const validatePrimeInput = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const { number } = req.query;
-  if (!number || typeof number !== "string") {
-    return res.status(400).json({
-      status: "error",
-      message: 'Invalid or missing "number" query parameter.',
-    });
+  try {
+    const { number } = req.query;
+    if (!number || typeof number !== "string") {
+      throw new ValidationError('Invalid or missing "number" query parameter.');
+    }
+
+    try {
+      // Attempt to convert to BigInt to validate the input
+      BigInt(number);
+    } catch (error) {
+      throw new ValidationError(
+        "Invalid input: The provided value cannot be converted to a number."
+      );
+    }
+
+    (req as PrimeRequest).query.number = number;
+    next();
+  } catch (error) {
+    next(error);
   }
-  (req as PrimeRequest).query.number = number;
-  next();
 };
